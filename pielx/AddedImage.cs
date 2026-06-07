143690123a0f08297b8f27affbcf79c0c29a3e3a
Burak Kızıldemir <legolas924687@gmail.com>
2026-06-07 23:13:51 +0300

    Add files via upload

---
+++ pielx/AddedImage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pielx
+{
+    public class AddedImage
+    {
+        public RGB imageRgb = new RGB();
+        public string imagePath { get; set; }
+        
+    }
+
+}
---
+++ pielx/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace pielx
+{
+    /// <summary>
+    /// App.xaml etkileşim mantığı
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
