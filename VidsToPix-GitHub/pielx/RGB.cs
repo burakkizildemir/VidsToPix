bcbc3a7adefde96d283b09b8e525c7a8d356740d
Burak Kızıldemir <legolas924687@gmail.com>
2026-06-07 23:18:42 +0300

    Add files via upload

---
+++ VidsToPix-GitHub/pielx/RGB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pielx
+{
+    public class RGB
+    {
+        public int Red { get; set; }
+        public int Green { get; set; }
+        public int Blue { get; set; }
+    }
+}
